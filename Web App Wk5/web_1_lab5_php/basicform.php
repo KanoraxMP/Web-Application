8caf2eb698fc978c7a7ad689de1752e4100db7e1
Prixestza <142098103+KanoraxMP@users.noreply.github.com>
2025-07-30 03:07:27 +0700

    Add files via upload

---
+++ Web App Wk5/web_1_lab5_php/basicform.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+
+        <form action="welcome.php" method="POST">
+            Name: <input type="text" name="name"><br>
+            E-mail: <input type="text" name="email"><br>
+            <input type="submit" value="OK">
+        </form>
+
+    </body>
+</html>
